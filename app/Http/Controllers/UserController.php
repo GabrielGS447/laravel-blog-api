4beb4fa35b31d28b18d407a0cd72f4738d4f6d70
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-23 15:58:45 -0300

    Create UserResource and UserCollection to transform JSON response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Resources\UserCollection;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
   public function index() {
-    return User::all();
+    return new UserCollection(User::all());
   }
 
   public function show(User $user) {
-    return $user;
+    return new UserResource($user);
   }
 }
