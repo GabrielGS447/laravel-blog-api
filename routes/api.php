251b8a8bc1423257c807c5f8fe61528dcb7d0b54
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-23 15:55:05 -0300

    Create GET /users/{user} endpoint

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
   Route::get('/users', 'index');
+  Route::get('/users/{user}', 'show');
 });
