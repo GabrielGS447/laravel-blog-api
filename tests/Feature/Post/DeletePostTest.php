6dcbefb87bce0df54a20896396b6eaa147447d31
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-29 11:22:41 -0300

    Create DeletePost tests

---
+++ tests/Feature/Post/DeletePostTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class DeletePostTests extends TestCase {
+  use RefreshDatabase;
+
+  public function test_delete_post() {
+    /** @var User */
+    $user = User::factory()->create();
+    $post = $user->posts()->create([
+      'title' => 'My first post',
+      'body' => 'This is my first post',
+    ]);
+
+    $response = $this->actingAs($user)->deleteJson("/api/posts/{$post->id}");
+
+    $response->assertNoContent();
+
+    $this->assertDatabaseMissing('posts', [
+      'id' => $post->id,
+      'title' => $post->title,
+      'body' => $post->body,
+      'user_id' => $user->id,
+    ]);
+  }
+
+  public function test_fail_to_delete_post_when_it_doesnt_belong_to_user() {
+    /** @var User */
+    $userOne = User::factory()->create();
+    $userTwo = User::factory()->create();
+    $post = $userTwo->posts()->create([
+      'title' => 'My first post',
+      'body' => 'This is my first post',
+    ]);
+
+    $response = $this->actingAs($userOne)->deleteJson("/api/posts/{$post->id}");
+
+    $response->assertForbidden();
+
+    $this->assertDatabaseHas('posts', [
+      'id' => $post->id,
+      'title' => $post->title,
+      'body' => $post->body,
+      'user_id' => $userTwo->id,
+    ]);
+  }
+
+  public function test_fail_to_delete_post_when_user_is_not_authenticated() {
+    /** @var User */
+    $user = User::factory()->create();
+    $post = $user->posts()->create([
+      'title' => 'My first post',
+      'body' => 'This is my first post',
+    ]);
+
+    $response = $this->deleteJson("/api/posts/{$post->id}");
+
+    $response->assertUnauthorized();
+
+    $this->assertDatabaseHas('posts', [
+      'id' => $post->id,
+      'title' => $post->title,
+      'body' => $post->body,
+      'user_id' => $user->id,
+    ]);
+  }
+}
