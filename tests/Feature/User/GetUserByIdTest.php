1e69ad43c0ade2093154d2528192d7dff8d72542
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-29 10:40:06 -0300

    Create GetUserById tests

---
+++ tests/Feature/User/GetUserByIdTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class GetUserByIdTests extends TestCase {
+  use RefreshDatabase;
+
+  public function test_get_user_by_id_without_including_posts() {
+    $users = User::factory()->create();
+
+    $response = $this->getJson("/api/users/{$users->id}");
+
+    $response
+      ->assertOk()
+      ->assertJson(fn (AssertableJson $json) =>
+        $json
+          ->where('data.id', $users->id)
+          ->where('data.displayName', $users->display_name)
+          ->where('data.email', $users->email)
+          ->missing('data.password')
+          ->missing('data.posts')
+          ->etc()
+      );
+  }
+
+  public function test_get_user_by_id_including_posts() {
+    $users = User::factory()->hasPosts(3)->create();
+
+    $response = $this->getJson("/api/users/{$users->id}?includePosts=true");
+
+    $response
+      ->assertOk()
+      ->assertJson(fn (AssertableJson $json) =>
+        $json
+          ->where('data.id', $users->id)
+          ->where('data.displayName', $users->display_name)
+          ->where('data.email', $users->email)
+          ->missing('data.password')
+          ->has('data.posts', 3)
+          ->has('data.posts.1', fn ($json) => 
+            $json
+              ->where('id', $users->posts[1]->id)
+              ->where('title', $users->posts[1]->title)
+              ->where('body', $users->posts[1]->body)
+              ->missing('user_id')
+              ->etc()
+          )
+          ->etc()
+      );
+  }
+}
